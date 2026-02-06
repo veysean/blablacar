f9d4b053591dc004906c5832d4edc76d6ca85bba
vesyean <veyseankem@gmail.com>
2026-02-06 10:34:50 +0700

    BLA-001 Rides Services (Correct)

--- lib/services/rides_service.dart
+++ lib/services/rides_service.dart
@@ -1,17 +1,41 @@
-import 'package:blabla/model/ride/locations.dart';
+import 'package:blabla/data/dummy_data.dart';
 
-import '../data/dummy_data.dart';
+import '../model/ride/locations.dart';
 import '../model/ride/ride.dart';
 
-////
-///   This service handles:
-///   - The list of available rides
-///
 class RidesService {
-  static List<Ride> allRides = fakeRides;
+  static List<Ride> availableRides = fakeRides; // TODO for now fake data
 
-  List<Ride> filterByDeparture(Location departure) {
-    final String filterDeparture;
-    return allRides.where((r) => r.allRide.departureLocation).toList();
+  //
+  //  filter the rides starting from given departure location
+  //
+  static List<Ride> _filterByDeparture(List<Ride> rides, Location departure) {
+    return rides.where((ride) => ride.departureLocation == departure).toList();
+  }
+
+  //
+  //  filter the rides starting for the given requested seat number
+  //
+  static List<Ride> _filterBySeatRequested(
+    List<Ride> rides,
+    int requestedSeat,
+  ) {
+    return rides.where((ride) => ride.availableSeats >= requestedSeat).toList();
+  }
+
+  //
+  //  filter the rides   with several optional criteria (flexible filter options)
+  //
+  static List<Ride> filterBy({Location? departure, int? seatRequested}) {
+    List<Ride> result = availableRides;
+
+    if (departure != null) {
+      result = _filterByDeparture(result, departure);
+    }
+    if (seatRequested != null) {
+      result = _filterBySeatRequested(result, seatRequested);
+    }
+
+    return result;
   }
 }
